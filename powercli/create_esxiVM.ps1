68a4530cfa9eb7cfb00eea764f9a5b7101b95024
root <root@ubuntu-12-04-1-x64.(none)>
2015-07-01 03:35:21 +0000

    added cd

--- powercli/create_esxiVM.ps1
+++ powercli/create_esxiVM.ps1
@@ -26,7 +26,7 @@ $VM_Name = $VMprefix + $y
 }
 
 Connect-VIServer -Server $esx -User $user -Password $password -ErrorAction Stop
-New-VM -VMHost $esx -Name $VM_Name -NumCpu 2 -MemoryGB 8 -DiskGB 8 -GuestId "rhel5_64Guest" -Datastore "400GB" -Version v9 -NetworkName "VM Network"
+New-VM -VMHost $esx -Name $VM_Name -NumCpu 2 -MemoryGB 8 -DiskGB 8 -GuestId "rhel5_64Guest" -Datastore "400GB" -Version v9 -NetworkName "VM Network" -CD:$True
 set-VM -VM  $VM_Name -GuestId "vmkernel5Guest" -Confirm:$False 
 start-vm -VM $VM_Name
 
