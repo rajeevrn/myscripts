56001789466ea8992267af3faa14f632702ab023
rajeev <nairrajivr.edu@gmail.com>
2018-01-03 16:45:20 +0530

    Moved and updated the file

--- vmware/vcenterapi.py
+++ vmware/vcenterapi.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python
 
 import json
-import  requests
+import requests
 import urllib3
 
-
+def apidata(url,restdata):
 #  Wanted to disable the warnings. Only for demo, do not disable the warnings. 
-urllib3.disable_warnings()
-
-#URL of the VCSA Appliance 
-url = "https://192.168.0.9/rest/appliance/"
-#API which you want to explore 
-restdata = "/health/applmgmt";
-#Verify=False - This makes the SSL verification as False
-rdata = requests.get(url + restdata, verify=False)
-
-#Reading the data and loading it as JSON so that it is easy to get the value 
-hdata = rdata.text
-mypar = json.loads(hdata)
-myvalue = mypar['value']
-
-#Checking the value
-print "The Health status of the appliance is:", myvalue.upper()
-if myvalue == "green":
-    print "VCSA health status is fine"        
-elif myvalue == "yellow":
-    print "Please check the Health Status, It is Yellow"
-else:
-    print "URGENT!!, Check the HealthStatus is Red"    
+   urllib3.disable_warnings()    
+   rdata = requests.get(url + restdata, verify=False)
+   hdata = rdata.text 
+   mypar = json.loads(hdata)
+   myvalue = mypar['value']
+   print "Current Status:", myvalue.upper()
+   if myvalue == "green":
+        print "VCSA Status is fine"
+   elif myvalue == "yellow":
+        print "Check the Status, It is Yellow"
+   else:
+        print "URGENT!!, Check the Status is Red"     
+   return
+   
+#Example to get the health status 
+  #1. URL of the VCSA Appliance
+  #2. restdata= Health status of the appliance
+   
+apidata("https://192.168.0.115/rest/appliance/","/health/applmgmt")
